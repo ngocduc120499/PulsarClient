5b61b5b65d8dc15c06f7caf14b8dcb5a25cdad48
nnduc <nnduc@tma.com.vn>
2021-09-09 10:34:19 +0700

    Update

--- src/main/java/Consumer/ExclusiveConsumer.java
+++ src/main/java/Consumer/ExclusiveConsumer.java
@@ -1,30 +1,33 @@
 package Consumer;
 
+import Model.Ticket;
 import Producer.Producer;
 import org.apache.pulsar.client.api.*;
 import Config.AppConfig;
+import org.apache.pulsar.client.impl.schema.JSONSchema;
+
+import java.util.List;
 
 public class ExclusiveConsumer {
     public static void main(String[] args) throws PulsarClientException {
         PulsarClient pulsarClient = PulsarClient.builder()
                                     .serviceUrl(AppConfig.SERVICE_URL)
                                     .build();
-        Consumer consumer = pulsarClient.newConsumer()
+        Consumer<Ticket> consumer = pulsarClient.newConsumer(Schema.JSON(Ticket.class))
                 .topic(AppConfig.TOPIC)
                 .consumerName("simple-consumer")
                 .subscriptionName("test-subs")
                 .subscriptionType(SubscriptionType.Exclusive)
                 .subscribe();
-        for(int i = 1; i<=60; i++){
-            Message msg = consumer.receive();
+        while (true){
+            Message<Ticket> msg = consumer.receive();
             try{
-                System.out.println("Received: " + new String(msg.getData()));
+                System.out.println("Received: " +new String(msg.getData()));
                 consumer.acknowledge(msg);
             }catch (Exception e){
                 consumer.negativeAcknowledge(msg);
             }
         }
-        consumer.close();
-        pulsarClient.close();
+
     }
 }
